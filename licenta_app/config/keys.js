7a3474e941b14572ea5cea49101dfcc0b9b107af
George Coltea <george.coltea@wizzdesign.ro>
2019-01-10 18:14:21 +0200

    added token generation

--- licenta_app/config/keys.js
+++ licenta_app/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://root:parola123@ds263367.mlab.com:63367/licenta_db"
+  mongoURI: "mongodb://root:parola123@ds263367.mlab.com:63367/licenta_db",
+  secretOrKey: "secret"
 };
