5e7f33bbb14e98d2391d4f33b09be54bc6ca9aec
George Coltea <george.coltea@wizzdesign.ro>
2019-01-13 21:43:50 +0200

    added add experience action

--- licenta_app/client/src/actions/profileAction.js
+++ licenta_app/client/src/actions/profileAction.js
@@ -38,6 +38,30 @@ export const createProfile = (profileData, history) => dispatch => {
     );
 };
 
+export const setProfileLoading = () => {
+  return {
+    type: PROFILE_LOADING
+  };
+};
+
+export const clearCurrentProfile = () => {
+  return {
+    type: CLEAR_CURRENT_PROFILE
+  };
+};
+
+export const addExperience = (expData, history) => dispatch => {
+  axios
+    .post("api/profile/experience", expData)
+    .then(res => history.push("dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 export const deleteAccount = () => dispatch => {
   if (window.confirm("Are you sure? This can NOT be undone!")) {
     axios
@@ -55,16 +79,4 @@ export const deleteAccount = () => dispatch => {
         })
       );
   }
-};
-
-export const setProfileLoading = () => {
-  return {
-    type: PROFILE_LOADING
-  };
-};
-
-export const clearCurrentProfile = () => {
-  return {
-    type: CLEAR_CURRENT_PROFILE
-  };
 };
